b74911150bf5c0eca55450fa0b8d5d4da3c4cfc1
VslTr <tri.vyacheslav@gmail.com>
2020-06-17 18:12:24 +0300

    2

--- go2type.go
+++ go2type.go
@@ -15,6 +15,11 @@ func main() {
 	var txt string
 	txt = "Gggg"
 	nm := 42.34
-	fmt.Println("nm is: ", reflect.TypeOf(nm), "\ntxt is: ", reflect.TypeOf(txt))
+	fmt.Println("nm = ", nm)
+	fmt.Println("nm is: ", reflect.TypeOf(nm))
+	fmt.Println("txt = ", txt, "\ntxt is: ", reflect.TypeOf(txt))
 	// TypeOf из пакета reflect  покажет тип переменной
+	nm2 := int(nm) // Преобразуем в int число с плавающей точкой nm
+	fmt.Println("nm2 = ", nm2)
+	fmt.Println("nm2 is: ", reflect.TypeOf(nm2))
 }
